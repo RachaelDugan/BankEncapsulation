1b2f7cc2d15070bbb6b539aa154cca544587c238
RachaelDugan <118020235+RachaelDugan@users.noreply.github.com>
2022-12-09 10:31:22 -0700

    initial commit

---
+++ BankEncapsulation/BankAccountClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccountClass
+    {
+        private double _balance = 0;
+        public BankAccountClass()
+        {
+        }
+        public void Deposit(double depositAmount)
+        {
+            _balance += depositAmount;
+        }
+        public double GetBalance() 
+        {
+            return _balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccountClass account = new BankAccountClass();
+
+            Console.WriteLine($"Current balance: ${account.GetBalance()}");
+            Console.WriteLine("Enter your deposit amount: ");
+            string deposit = Console.ReadLine();
+            account.Deposit(double.Parse(deposit));
+            Console.WriteLine($"Amount added to your account: {deposit, 0:c}");
+            Console.WriteLine($"New balance: {account.GetBalance()}");
+            
+            Console.WriteLine("Enter your deposit amount: ");
+            deposit = Console.ReadLine();
+            account.Deposit(double.Parse(deposit));
+            Console.WriteLine($"Amount added to your account: {deposit,0:c}");
+            Console.WriteLine($"New balance: {account.GetBalance()}");
+
         }
     }
 }
